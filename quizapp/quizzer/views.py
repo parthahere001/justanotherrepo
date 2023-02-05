09ac0eba9ddce97afe9de532a37eb68f739a0fc8
pm-2001 <princemauryapm2001@gmail.com>
2023-02-05 20:36:51 +0530

    added some templates

--- quizapp/quizzer/views.py
+++ quizapp/quizzer/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render (request,'home.html')
+
+def quize(request):
+    return render (request,'quize.html')
